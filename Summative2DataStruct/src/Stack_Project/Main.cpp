3f57c66dbe13a631e119f738d8d2cc23a9db7eeb
MG <mg-git.plumbic@8shield.net>
2021-02-21 16:39:54 +0800

    Fixed spacing

--- Summative2DataStruct/src/Stack_Project/Main.cpp
+++ Summative2DataStruct/src/Stack_Project/Main.cpp
@@ -6,16 +6,13 @@ using namespace std;
 
 int main()
 {
-	
-	GenericStack<int> intStack(2);
-	int intCatchVar;
-	
-	
-	cout << " ===================================\n";
-	cout << " |            INT  STACK             |\n";
+	cout << " ====================================\n";
+	cout << " |            INT  STACK            |\n";
 	cout << " ====================================\n";
 	cout << "\n";
 
+	GenericStack<int> intStack(2);
+	int intCatchVar;
 	
 	// OUTPUT INT STACK
 	intStack.push(5);
@@ -29,69 +26,62 @@ int main()
 	intStack.pop(intCatchVar);
 	cout << "POPPING " << intCatchVar << "\n";
 
-
+	
 	cout << " =====================================\n";
-	cout << " |          DOUBLE  STACK             |\n";
+	cout << " |            DOUBLE  STACK          |\n";
 	cout << " =====================================\n";
 	cout << "\n";
 
-	//OUTPUT DOUBLE STACK
 	GenericStack<double> doubleStack(5);
 	double doubleCatchVar;
-
+	
+	//OUTPUT DOUBLE STACK
 	doubleStack.push(5.5);
 	cout << "PUSHING 5.5 \n";
 	doubleStack.push(10.5);
 	cout << "PUSHING 10.5 \n ";
 	
-
-
 	cout << "\n";
 	doubleStack.pop(doubleCatchVar);
 	cout << "POPPING " <<  doubleCatchVar << "\n";
 	doubleStack.pop(doubleCatchVar);
 	cout << "POPPING " << doubleCatchVar << "\n";
 
-
-
-	cout << " =====================================\n";
-	cout << " |          CHAR  STACK               |\n";
-	cout << " =====================================\n";
+	
+	cout << " ====================================\n";
+	cout << " |            CHAR  STACK           |\n";
+	cout << " ====================================\n";
 	cout << "\n";
 
-	//OUTPUT CHAR STACK
 	GenericStack<char> charStack(5);
 	char charCatchVar;
-
+	
+	//OUTPUT CHAR STACK
 	charStack.push('A');
 	cout << "PUSHING A \n";
 	charStack.push('B');
 	cout << "PUSHING B \n ";
 
-
-
 	cout << "\n";
 	charStack.pop(charCatchVar);
 	cout << "POPPING " << charCatchVar << "\n";
 	charStack.pop(charCatchVar);
 	cout << "POPPING " << charCatchVar << "\n";
-
-
-	//OUTPUT STRING STACK
-	GenericStack<string> stringStack(5);
-	string stringCatchVar;
+	
 
 	cout << " =====================================\n";
 	cout << " |          STRING  STACK             |\n";
 	cout << " =====================================\n";
 	cout << "\n";
 
-
+	GenericStack<string> stringStack(5);
+	string stringCatchVar;
+	
+	//OUTPUT STRING STACK
 	stringStack.push("HELLO WORLD");
 	cout << "PUSHING HELLO WORLD \n";
 	stringStack.push("WELCOME BACK");
 	cout << "PUSHING WELCOME BACK \n ";
-
 
 	cout << "\n";
 	stringStack.pop(stringCatchVar);
@@ -99,12 +89,6 @@ int main()
 	stringStack.pop(stringCatchVar);
 	cout << "POPPING " << stringCatchVar << "\n";
 
-
-	
-
-
-
-	
 	return 0;
 
 }
