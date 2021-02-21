fc6d8e6c0243813c8086c309ba47bb0afa8e83a2
Dave Bornilla <T201911410@feucanvas.edu.ph>
2021-02-21 17:20:45 +0800

    Added cout on enqueing x

--- Summative2DataStruct/src/Queue_Project/Main.cpp
+++ Summative2DataStruct/src/Queue_Project/Main.cpp
@@ -18,11 +18,13 @@ int main()
 		cout << "Enqueuing 5 items...\n";
 		// Enqueue 5 items.
 		for (int x = 0; x < 5; x++)
+		{
+			cout << "Pushing " << x << '\n';
 			iQueue.enqueue(x);
-
+		}
 		// Attempt to enqueue while full.
-		// cout << "Now attempting to enqueue again...\n";
-		// iQueue.enqueue(5);
+		 cout << "Now attempting to enqueue again...\n";
+		 iQueue.enqueue(5);
 
 	}
 	catch (runtime_error &errorName) {
